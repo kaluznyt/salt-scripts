4d2747f01c2241c3df93e71babc27482e2c12f25
Tomasz <tkaluzny@outlook.com>
2018-01-01 23:35:02 +0100

    changes

--- salt-minion-vagrant-provisioner-windows.ps1
+++ salt-minion-vagrant-provisioner-windows.ps1
@@ -1,7 +1,7 @@
 # https://docs.saltstack.com/en/latest/topics/installation/windows.html
 # https://www.vagrantup.com/docs/provisioning/shell.html
 PARAM($masterName, $minionName, $minionInstallerUrl)
-{
+
 	$saltMinionInstallerExe = "Salt-Minion-2017.7.2-Py2-AMD64-Setup.exe"
 	$saltMinionInstallerLocalPath = "C:\tmp\$saltMinionInstallerExe"
 	#$saltMinionInstallerUrl = "https://repo.saltstack.com/windows/$saltMinionInstallerExe"
@@ -18,6 +18,6 @@ PARAM($masterName, $minionName, $minionInstallerUrl)
 	Start-Process $saltMinionInstallerLocalPath -ArgumentList "/S /master=$masterName /minion-name=$minionName /start-minion=1" -Wait
 	
 	Write-Host "Installation successful !"
-}		
+		
 
 
