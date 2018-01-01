a7be3a852d468553fa393da361fd946988503692
Tomasz <tkaluzny@outlook.com>
2018-01-01 23:37:49 +0100

    changes

--- salt-minion-vagrant-provisioner-windows.ps1
+++ salt-minion-vagrant-provisioner-windows.ps1
@@ -2,22 +2,22 @@
 # https://www.vagrantup.com/docs/provisioning/shell.html
 PARAM($masterName, $minionName, $minionInstallerUrl)
 
-	$saltMinionInstallerExe = "Salt-Minion-2017.7.2-Py2-AMD64-Setup.exe"
-	$saltMinionInstallerLocalPath = "C:\tmp\$saltMinionInstallerExe"
-	#$saltMinionInstallerUrl = "https://repo.saltstack.com/windows/$saltMinionInstallerExe"
+$saltMinionInstallerExe = "Salt-Minion-2017.7.2-Py2-AMD64-Setup.exe"
+$saltMinionInstallerLocalPath = "C:\tmp\$saltMinionInstallerExe"
+#$saltMinionInstallerUrl = "https://repo.saltstack.com/windows/$saltMinionInstallerExe"
 
-	Write-Host "Downloading Minion setup from $minionInstallerUrl"	
-	
-	if(!(Test-Path $saltMinionInstallerLocalPath))
-	{
-		Start-BitsTransfer -Source $minionInstallerUrl -Destination $saltMinionInstallerLocalPath
-	}
+Write-Host "Downloading Minion setup from $minionInstallerUrl"	
 
-	Write-Host "Installing Minion..."	
+if(!(Test-Path $saltMinionInstallerLocalPath))
+{
+	Start-BitsTransfer -Source $minionInstallerUrl -Destination $saltMinionInstallerLocalPath
+}
+
+Write-Host "Installing Minion..."	
+
+Start-Process $saltMinionInstallerLocalPath -ArgumentList "/S /master=$masterName /minion-name=$minionName /start-minion=1" -Wait
+
+Write-Host "Installation successful !"
 	
-	Start-Process $saltMinionInstallerLocalPath -ArgumentList "/S /master=$masterName /minion-name=$minionName /start-minion=1" -Wait
-	
-	Write-Host "Installation successful !"
-		
 
 
